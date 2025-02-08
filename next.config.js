b7a863bfdb85aa2a02e04352a915621b8f0e26ee
lonestarx1 <siradriel@gmail.com>
2025-02-08 14:53:53 +0900

    fix netlify next js build issue

--- next.config.js
+++ next.config.js
@@ -2,7 +2,9 @@
 const nextConfig = {
   reactStrictMode: true,
   trailingSlash: true,
+  output: "export",
   images: {
+    unoptimized: true,
     domains: [], // Add any external image domains if needed
     deviceSizes: [640, 750, 828, 1080, 1200, 1920, 2048, 3840],
     imageSizes: [16, 32, 48, 64, 96, 128, 256, 384],
