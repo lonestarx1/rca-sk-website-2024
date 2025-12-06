81dca04835825f7cd5e102c9d50d240bb2d1d8b8
lonestarx1 <siradriel@gmail.com>
2025-12-06 03:30:58 -0800

    udd useful links

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -28,7 +28,42 @@ const Footer = () => {
           >
              <span>📸</span> Instagram
           </a>
-          {/* Add more social links here if needed */}
+          <a 
+            href="https://x.com/InRwandan?t=sDncp-E4hYYTHlmCOxYfsQ&s=08" 
+            target="_blank" 
+            rel="noopener noreferrer"
+            className={styles.socialLink}
+          >
+             <span>✖️</span> X
+          </a>
+        </div>
+
+        <div className={`${styles.column} ${styles.usefulLinks}`}>
+          <h3>Useful Links</h3>
+          <a 
+            href="https://www.rwandainkorea.gov.rw/" 
+            target="_blank" 
+            rel="noopener noreferrer"
+            className={styles.socialLink}
+          >
+             <span>🇷🇼</span> Embassy Website
+          </a>
+          <a 
+            href="https://forms.gle/aRmvETMJJNsS6jpz9" 
+            target="_blank" 
+            rel="noopener noreferrer"
+            className={styles.socialLink}
+          >
+             <span>📦</span> Document Delivery
+          </a>
+          <a 
+            href="https://forms.gle/WVdPfSTsG8cMyN4H6" 
+            target="_blank" 
+            rel="noopener noreferrer"
+            className={styles.socialLink}
+          >
+             <span>📝</span> Submit a Claim
+          </a>
         </div>
       </div>
       <div className={styles.copyright}>
